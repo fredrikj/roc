04bad603e0eca1bd25eb30ce582192ad3fc73716
Fredrik Johansson <fred@fred-desktop.(none)>
2009-10-21 18:54:34 +0900

    Replaced test file with the correct one...

--- test/roc.rb
+++ test/roc.rb
@@ -4,7 +4,7 @@ $:.unshift('..')
 $:.unshift('../lib')
 
 require('test/unit')
-require('forkoff')
+require('roc')
 
 
 class T < Test::Unit::TestCase
@@ -13,42 +13,8 @@ class T < Test::Unit::TestCase
 #
 
   def test_0010
-    results = [0,1,2].forkoff!{|n| [n, Process.pid]}
-    assert_equal(results, results.uniq, 'all ran in a different process')
+    r = ROCarray.new( %w(P N P) )
+    assert r.auc == 0.5
   end
-
-# it's faster
-#
-  def test_0020
-    n = 4
-    strategies = :forkoff, :each
-
-    4.times do
-      result = {}
-
-      strategies.each do |strategy|
-        a = Time.now.to_f
-        (0..4).send(strategy){|i| sleep 0.2}
-        b = Time.now.to_f
-        elapsed = b - a
-        result[strategy] = elapsed
-      end
-
-      assert result[:forkoff] < result[:each], 'forkoff is faster than each for long running tasks'
-    end
-  end
-
-# in case of different execution times for different processes
-#
-  def test_0030
-    a = Time.now.to_f
-    (0...4).forkoff(2) do |i|
-      sleep i.modulo(2)
-    end
-    b = Time.now.to_f
-    elapsed = b - a
-    assert elapsed < 2
-  end
-
 
 end
